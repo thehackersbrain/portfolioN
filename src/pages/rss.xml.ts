c7e516893564fda3b8d0a0c3ba69d84685ea718c
Gaurav Raj <techw803@gmail.com>
2025-12-28 06:09:36 +0530

    fixed rss feed atom error

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -6,22 +6,17 @@ import { getAllPosts } from '@/lib/data-utils'
 export async function GET(context: APIContext) {
   try {
     const posts = await getAllPosts()
-    const siteUrl = context.site ?? SITE.href
-    
+
     return rss({
       title: SITE.title,
       description: SITE.description,
-      site: siteUrl,
+      site: context.site ?? SITE.href,
       items: posts.map((post) => ({
         title: post.data.title,
         description: post.data.description,
         pubDate: post.data.date,
         link: `/blog/${post.id}/`,
       })),
-      customData: `<atom:link href="${siteUrl}/rss.xml" rel="self" type="application/rss+xml" />`,
-      xmlns: {
-        atom: "http://www.w3.org/2005/Atom",
-      },
     })
   } catch (error) {
     console.error('Error generating RSS feed:', error)
